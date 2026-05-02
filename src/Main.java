2b7dcb7b3b8062d47495037ee900b0038ec896d5
ramthota3 <ramthota486@gmail.com>
2026-05-02 16:37:11 +0530

    java2026.iml

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args){
+        String studentName = "Ram";
+        int studentId = 3;
+        int studentAge = 27;
+        float studentFee = 75.25f;
+        char studentGrade = 'A';
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Student name: "+ studentName);
+        System.out.println("Student id: "+ studentId);
+        System.out.println("Student age: "+ studentAge);
+        System.out.println("Student fee: "+ studentFee);
+        System.out.println("Student grade:"+ studentGrade);
     }
 }
