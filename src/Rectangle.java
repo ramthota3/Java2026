d8c5eb49ce330a4f57338390e504765d6315fc2d
ramthota3 <ramthota486@gmail.com>
2026-05-02 16:37:06 +0530

    java2026.iml

---
+++ src/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    public static void main(String[] args){
+        int length = 6;
+        int width = 8;
+        int area;
+        area = length * width;
+        System.out.println("length is :" + length);
+        System.out.println("width is :" + width);
+        System.out.println("Area of rectangle is :"+ area);
+    }
+}
