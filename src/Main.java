5b57e887a8d7c71130e01876899d5609a65b398e
ramthota3 <ramthota486@gmail.com>
2026-05-04 12:30:52 +0530

    added two programs

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
 public class Main {
     public static void main(String[] args){
-        String studentName = "Ram";
-        int studentId = 3;
-        int studentAge = 27;
-        float studentFee = 75.25f;
-        char studentGrade = 'A';
+//        String studentName = "Ram";
+//        int studentId = 3;
+//        int studentAge = 27;
+//        float studentFee = 75.25f;
+//        char studentGrade = 'A';
+//        char studentGrades = 'B';
+//
+//
+//        System.out.println("Student name: "+ studentName);
+//        System.out.println("Student id: "+ studentId);
+//        System.out.println("Student age: "+ studentAge);
+//        System.out.println("Student fee: "+ studentFee);
+//        System.out.println("Student grade:"+ studentGrade);
+//        System.out.println("Student grades:"+ studentGrades);
 
-        System.out.println("Student name: "+ studentName);
-        System.out.println("Student id: "+ studentId);
-        System.out.println("Student age: "+ studentAge);
-        System.out.println("Student fee: "+ studentFee);
-        System.out.println("Student grade:"+ studentGrade);
+
+//        int a = 4;
+//        int b = 6;
+//
+//        System.out.println("Addtion: " + (a+b));
+//        System.out.println("Subtraction: " + (a-b));
+//        System.out.println("Multiplication: "+ (a*b));
+//        System.out.println("Division: "+ (a/b));
+//        System.out.println("modulus: "+ (a%b));
+
+        int a = 11;
+        int b = 5;
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a >= b:" + (a >= b));
+        System.out.println("a == b:" + (a == b));
+        System.out.println("a != b:" + (a != b));
+
     }
 }
